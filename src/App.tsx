829e2af36e1841301b0c1ad06a45c5e0d9d558f8
Kamol <kamolkhamidov200002@gmail.com>
2023-02-12 20:13:50 +0500

    add main layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,31 @@
 export function App() {
   return <div></div>;
 }
+  return (
+        <Paper
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            height: '100vh',
+            bgColor: 'black',
+            borderRadius: 0,
+          }}
+        >
+          <Paper
+            sx={{
+              height: '800px',
+              overflowY: 'auto',
+              '::-webkit-scrollbar': {
+                display: 'none',
+              },
+              padding: '20px',
+              margin: '20px',
+              borderRadius: '20px',
+              bgcolor: 'primary.main',
+            }}
+          >
+          </Paper>
+        </Paper>
+  );
+}
